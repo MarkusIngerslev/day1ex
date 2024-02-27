c893196ef1a56efa2b43cdb0fbbeb7c6e95ccd61
MarkusIngerslev <markusolsen2004@gmail.com>
2024-02-27 14:27:09 +0100

    Forms uncontrolled løsning 3 useRef Hook

--- src/components/UserForm.tsx
+++ src/components/UserForm.tsx
@@ -1,5 +1,5 @@
 import { BaseProps } from "../types";
-import { FormEvent, useState } from "react";
+import { FormEvent, useRef } from "react";
 import { User } from "../data/data";
 
 type UserFormProps = BaseProps & {
@@ -7,12 +7,15 @@ type UserFormProps = BaseProps & {
 };
 
 export default function UserForm({ title, onSubmitUser }: UserFormProps) {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [isActive, setIsActive] = useState(false);
+    const nameRef = useRef<HTMLInputElement>(null);
+    const emailRef = useRef<HTMLInputElement>(null);
+    const isActiveRef = useRef<HTMLInputElement>(null);
 
     const onSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        const name = nameRef.current?.value || "";
+        const email = emailRef.current?.value || "";
+        const isActive = isActiveRef.current?.checked || false;
         onSubmitUser({ name, email, isActive });
     };
 
@@ -20,15 +23,9 @@ export default function UserForm({ title, onSubmitUser }: UserFormProps) {
         <>
             <h2>{title}</h2>
             <form onSubmit={onSubmit}>
-                First Name: <input name="name" value={name} onChange={(e) => setName(e.target.value)} />
-                Email: <input name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                Active:{" "}
-                <input
-                    type="checkbox"
-                    name="isActive"
-                    checked={isActive}
-                    onChange={(e) => setIsActive(e.target.checked)}
-                />
+                First Name: <input name="name" ref={nameRef} />
+                Email: <input name="email" ref={emailRef} />
+                Active: <input type="checkbox" name="isActive" ref={isActiveRef} />
                 <button type="submit">Add User</button>
             </form>
         </>
